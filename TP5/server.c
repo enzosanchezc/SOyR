95339559e8744729b30f8e312653c9751baebdd5
enzosanchezc <enzosanchezcroce@hotmail.com>
2022-02-17 20:29:29 -0300

    Correción de bugs

--- TP5/server.c
+++ TP5/server.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <time.h>
+#include <sys/wait.h>
 #define PORT 8080
 
 int main(int argc, char const *argv[])
@@ -20,7 +21,9 @@ int main(int argc, char const *argv[])
     FILE *log_fd;
 
     time_t rawtime;
-    struct tm *starttime, *endtime;
+    char starttime[64], endtime[64];
+
+    int child_count = 0;
 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
@@ -43,7 +46,7 @@ int main(int argc, char const *argv[])
 
     listen(server_fd, 10);
 
-    while (1)
+    while (10 - child_count)
     {
         if ((socket_con = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
         {
@@ -51,9 +54,10 @@ int main(int argc, char const *argv[])
             exit(EXIT_FAILURE);
         }
         printf("Conexión entrante desde %s:%d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+        child_count++;
 
         time(&rawtime);
-        starttime = localtime(&rawtime);
+        strcpy(starttime, asctime(localtime(&rawtime)));
         
         int bytesrecv = 0;
         int bytessent = 0;
@@ -62,12 +66,14 @@ int main(int argc, char const *argv[])
         if (PID == 0)
         {
             close(server_fd);
-            bytessent += send(socket_con, "listo", 6, 0);
+            bytessent += send(socket_con, "Listo", 6, 0);
 
             // Espera el comando archivo <nombre del archivo>
             bytesrecv += recv(socket_con, buffer, 1024, 0);
             char *filename = strtok(buffer, " ");
             filename = strtok(NULL, " ");
+            char _filename[strlen(filename)];
+            strcpy(_filename, filename);
 
             // Espera el tamaño del archivo
             bytesrecv += recv(socket_con, buffer, 1024, 0);
@@ -77,27 +83,38 @@ int main(int argc, char const *argv[])
             int filesize = 0;
             while (filesize < expectedfilesize)
             {
-                // BUG: Si se pone un tamaño esperado mayor al real se cuelga
                 int n = recv(socket_con, buffer, 1024, 0);
                 fwrite(buffer, sizeof(char), n, fd);
                 filesize += n;
+                if (n < 1024)
+                    break;
             }
             fclose(fd);
 
             memset(buffer, 0, 1024);
-            sprintf(buffer, "Archivo %s completo, tamaño declarado %d bytes, tamaño real %d bytes.", filename, expectedfilesize, filesize);
+            sprintf(buffer, "Archivo %s completo, tamaño declarado %d bytes, tamaño real %d bytes.", _filename, expectedfilesize, filesize);
             printf("%s\n", buffer);
             bytessent += send(socket_con, buffer, strlen(buffer), 0);
 
             time(&rawtime);
-            endtime = localtime(&rawtime);
-            log_fd = fopen("log.csv", "w");
-            fprintf(log_fd, "%s:%d,%d,%d,%d,%s,%s\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port), filesize, bytessent, bytesrecv + filesize, asctime(starttime), asctime(endtime));
+            strcpy(endtime, asctime(localtime(&rawtime)));
+
+            endtime[strlen(endtime) - 1] = '\0';
+            starttime[strlen(starttime) - 1] = '\0';
+
+            log_fd = fopen("log.csv", "a");
+            fprintf(log_fd, "%s:%d,%d,%d,%d,%s,%s\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port), filesize, bytessent, bytesrecv + filesize, starttime, endtime);
             fclose(log_fd);
             
             close(socket_con);
             break;
         }
+    }
+    
+    // verificar que no queden procesos zombie
+    for (int i = 0; i < child_count; i++)
+    {
+        wait(NULL);
     }
 
     return 0;
